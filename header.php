ca145e7450887f80c6f9246fb56aa5be6aebd903
Mahmoud Maguid <mahmoud.maguid@yahoo.ca>
2020-05-04 20:33:36 -0600

    v1

---
+++ header.php
@@ -0,0 +1,41 @@
+Hello World !
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+      
+    <?php
+      // Output our site title.
+      bloginfo( 'name' );
+      // Show page info (if available.)
+      if ( $current_post_title = get_the_title() ) {
+        echo ' | ' . $current_post_title;
+      }
+    ?>
+  </title>
+  <?php wp_head();?>
+</head>
+<body>
+  <h1>
+    <?php
+      // Output our site title.
+      bloginfo( 'name' );
+      // Show page info (if available.)
+      if ( $current_post_title = get_the_title() ) {
+        echo ' | ' . $current_post_title;
+      }
+    ?>
+  </h1>
+  <nav>
+      <h2>  website main menu (Main MENUE)</h2>
+      <?php     wp_nav_menu(
+          
+        array(
+
+            'main menue' => 'Main menu'
+        )
+        );
+          ?>
+  </nav>
